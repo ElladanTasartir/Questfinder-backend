96ddf80a55a5bbcce3d9895466fd936f0a2dddd2
Erick Malta <erickmalta100@gmail.com>
2020-05-31 11:48:59 -0300

    Eslint and Prettier configured

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-const express = require('express');
+/* eslint no-console: ["error", { allow: ["warn", "error", "log"]}] */
+const express = require("express");
+
 const port = process.env.PORT || 3333;
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('<h1>Hello Questfinder!</h1>');
+app.get("/", (req, res) => {
+  res.send("<h1>Hello Questfinder!</h1>");
 });
 
 app.listen(port, () => {
-    console.log(`Servidor iniciado na porta ${port}`);
-    console.log(`Acesso em http://localhost:${port}`);
+  console.log(`Servidor iniciado na porta ${port}`);
+  console.log(`Acesso em http://localhost:${port}`);
 });
