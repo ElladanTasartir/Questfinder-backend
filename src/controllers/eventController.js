3bbcfbd842993630ce6707c7e0fbd25fad17a32a
vinicius-gandini <viniciusgandini.goncalves@hotmail.com>
2020-06-17 22:05:32 -0300

    Adicionado método para inativar evento :ok_hand:

--- src/controllers/eventController.js
+++ src/controllers/eventController.js
@@ -15,9 +15,25 @@ const create = async (req, res, next) => {
 const alter = async (req, res, next) => {
   try {
     const alterEvent = new Event(req.body);
+    return alterEvent;
   } catch (err) {
     next(err);
   }
 };
 
-module.exports = { create, alter };
+// Método para inativar o evento
+const inativate = async (req, res, next) => {
+  try {
+    const event = new Event();
+
+    // Variável que recebe o retorno da chamada à função de inativação do evento
+    const inativated = await event.inativate(req.params.id);
+
+    // Após alteração, retorna o status de sucesso com o evento alterado
+    return res.status(200).json({ event: inativated });
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports = { create, alter, inativate };
